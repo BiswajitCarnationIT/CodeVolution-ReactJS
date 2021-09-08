962b84297e55b5be4e1d6edccd8aea2551f45942
Biswajit <biswajit.majumdar@carnationit.com>
2021-09-08 22:16:31 +0530

    Lecture17 continuing after personList

--- src/Tutorial17.js
+++ src/Tutorial17.js
@@ -1,14 +1,14 @@
 /*   -----List rendering ----- */
 
 import './App.css';
-import NameList from './components/NameList'; 
+import PersonList from './components/PersonList'; 
 
 
 
 function App() {
     return (
       <div className="App">
-          <NameList/>
+          <PersonList/>
      
       </div>
     );
